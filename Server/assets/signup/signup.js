e9eb29dfe6aa4f31be04bf8071f9bbf9a008b5af
Spandan Goel <52139195+Spandan14@users.noreply.github.com>
2021-04-17 14:38:04 -0500

    finished all the main pages

--- Server/assets/signup/signup.js
+++ Server/assets/signup/signup.js
@@ -1,9 +1,40 @@
 
-function login() {
+function signup() {
     const data = {                                                  // grab form data
         username: document.getElementById('username').value,
         password: document.getElementById('password').value,
+        confirmPassword: document.getElementById('confirmpassword').value,
     }
+
+    let error = false;
+
+    if (data.username === "") {
+        document.getElementById('usernameTooltip').textContent = "Username cannot be blank!"
+        document.getElementById('usernameTooltip').style.visibility = "visible";
+        error = true;
+    }
+    else {
+        document.getElementById('usernameTooltip').style.visibility = "hidden";
+    }
+    if (data.password.length < 8) {
+        document.getElementById('passwordTooltip').style.visibility = "visible";
+        error = true;
+    }
+    else {
+        document.getElementById('passwordTooltip').style.visibility = "hidden";
+    }
+    if (data.confirmPassword !== data.password) {
+        document.getElementById('confirmPasswordTooltip').style.visibility = "visible";
+        error = true;
+    }
+    else {
+        document.getElementById('confirmPasswordTooltip').style.visibility = "hidden";
+    }
+    if (error) {
+        return
+    }
+
+
 
     var xhr = new XMLHttpRequest();                                 // create http request and send it
     xhr.open("POST", "./", true);
@@ -16,7 +47,8 @@ function login() {
             if (response === 'error') {                            // alert when there is an issue or username exists
                 alert('There was an error with creating your account, we apologize for the inconvinience')
             } else if (response === 'exists') {
-                alert('A user with that username already exists!')
+                document.getElementById('usernameTooltip').textContent = "An account with this username already exists!"
+                document.getElementById('usernameTooltip').style.visibility = "visible"
             } else {
                 document.cookie = "session=" + response + "; path=/; httpOnly: false";  // otherwise, set session cookie and redirect to profile
                 window.location.replace('../profile')
