c5782c7c9d7e4178a812d5ace3da13837edc918f
unknown <Bwu1324@gmail.com>
2021-04-16 20:33:59 -0500

    js for login

--- login/login.js
+++ login/login.js
@@ -1,4 +1,13 @@
 
 function login () {
-    document.getElementByID()
+    var username = document.getElementById('username')
+    var password = document.getElementByID('password')
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "./login", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        username: username,
+        password: password,
+    }));
 }
