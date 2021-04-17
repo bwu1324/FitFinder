52388cf7f378ed73030fcf3bae90e271eb21be97
unknown <Bwu1324@gmail.com>
2021-04-17 01:52:42 -0500

    Backend authentication for login and profile pages done, session cookies done, login signup forms done

--- Server/assets/signup/signup.js
+++ Server/assets/signup/signup.js
@@ -1,13 +1,26 @@
 
-function login () {
-    var username = document.getElementById('username').value
-    var password = document.getElementById('password').value
-    
-    var xhr = new XMLHttpRequest();
+function login() {
+    const data = {                                                  // grab form data
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value,
+    }
+
+    var xhr = new XMLHttpRequest();                                 // create http request and send it
     xhr.open("POST", "./", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        username: username,
-        password: password
-    }));
+    xhr.send(JSON.stringify(data));
+
+    xhr.onreadystatechange = function () {                           // wait for response
+        if (xhr.readyState == 4) {
+            const response = xhr.responseText
+            if (response === 'error') {                            // alert when there is an issue or username exists
+                alert('There was an error with creating your account, we apologize for the inconvinience')
+            } else if (response === 'exists') {
+                alert('A user with that username already exists!')
+            } else {
+                document.cookie = "session=" + response + "; path=/; httpOnly: false";  // otherwise, set session cookie and redirect to profile
+                window.location.replace('../profile')
+            }
+        }
+    };
 }
--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const crypto = require('crypto');
-const argon2 = require('argon2');
-const fs = require('fs');
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const crypto = require('crypto')
+const argon2 = require('argon2')
+const fs = require('fs')
 
-const secret = crypto.randomBytes(16);
+const secret = '0439868ec28dab59' //crypto.randomBytes(16)
 
-function decryptCookie (data) {
+function decryptCookie(data) {              // takes in session cookie, returns stringified json data
     try {
         data = JSON.parse(Buffer.from(data, 'base64').toString('ascii'))
         const iv = Buffer.from(data.iv)
@@ -17,11 +17,11 @@ function decryptCookie (data) {
         decrypted += decipher.final('utf8')
         return decrypted
     } catch (error) {
-        return error
+        return undefined
     }
 }
 
-function encryptCookie (data) {
+function encryptCookie(data) {              // takes in stringified user data, returns session cookie
     try {
         const iv = crypto.randomBytes(16)
         const cipher = crypto.createCipheriv('aes-128-cbc', secret, iv)
@@ -30,54 +30,118 @@ function encryptCookie (data) {
         encrypted += cipher.final('hex')
         return Buffer.from(JSON.stringify({ data: encrypted, iv: iv })).toString('base64')
     } catch (error) {
-        return error
+        return undefined
     }
 }
 
-function createCookie (user) {
+function createCookie(user) {               // takes in username, returns session cookie
     const data = JSON.parse(fs.readFileSync('./userData/' + user + '.json'))
     const userData = {
-
+        username: data.username,
+        hash: data.hash
     }
-    return encryptCookie (userData)
+    return encryptCookie(JSON.stringify(userData))
 }
 
-const app = express();
+function findUser(session) {                // takes in session cookie, return promise. resolves to user object if found, undefined it not
+    return new Promise((resolve) => {
+        try {
+            const user = JSON.parse(decryptCookie(session))
+            fs.access('./userData/' + user.username + '.json', (err) => {
+                if (err) {
+                    resolve(undefined)
+                    return
+                }
+                fs.readFile('./userData/' + user.username + '.json', (error, data) => {
+                    if (error) {
+                        resolve(undefined)
+                        return
+                    }
+                    resolve(JSON.parse(data))
+                })
+            })
+        } catch { resolve(undefined) }
+    })
+}
 
-app.set('view engine', 'ejs');
+const app = express()
 
-app.use(express.static('./assets'));
-app.use(cookieParser());
-app.use(bodyParser.json());
+app.set('view engine', 'ejs')
 
-app.get('/login', (req, res) => res.render('login'));
-app.get('/signup', (req, res) => res.render('signup'));
+app.use(express.static('./assets'))
+app.use(cookieParser())
+app.use(bodyParser.json())
 
 
-app.post('/login', (req, res) => {
-    const data = req.body
-    console.log(data)
+app.get('/index', (req, res) => {            // homepage
+    res.render('index')
 })
 
-app.post('/signup', async (req, res) => {
-    const data = req.body
+app.get('/login', async (req, res) => {      // login page
+    const user = await findUser(req.cookies.session)    // check if user has valid session cookie, redirect to profile if yes
+    if (user) { res.redirect('/profile') }
+    else { res.render('login') }                        // otherwise, render page
+})
 
-    try {
-        const hash = await argon2.hash(data.password);
+app.get('/signup', async (req, res) => {
+    const user = await findUser(req.cookies.session)    // check if user has valid session cookie, redirect to profile if yes
+    if (user) { res.redirect('/profile') }
+    else { res.render('signup') }                       // otherwise, render page
+})
 
-        const newUser = {
-            username: data.username,
-            hash: hash,
-            name: data.name
+app.get('/profile', async (req, res) => {
+    const user = await findUser(req.cookies.session)    // check if user has valid session cookie, redirect to profile if yes
+    if (user) { res.render('profile', { user: user }) }
+    else { res.redirect('/index') }                     // otherwise, redirect to index
+})
+
+
+app.post('/login', async (req, res) => {                // login form post req
+    const data = req.body                                   // grab the data
+    fs.readFile('./userData/' + data.username + '.json', async (err, user) => {
+        if (err) {                                          // read the file, if error, it doesn't exist, send fail
+            res.send('fail')
+            return
         }
+        try {                                               // otherwise, check if passwords match
+            const hash = JSON.parse(user).hash                  // hash stored in database
 
-        fs.writeFile('./userData/' + data.username + '.json', JSON.stringify(newUser), (error) => {
-            if (error) { res.render('error', { type: 'writefailed' }) }
+            if (await argon2.verify(hash, data.password)) {     // compare
+                res.send(createCookie(data.username))               // if success, send session cookie
 
-            res.cookie('session', createCookie(data.username) )
-            res.render('profile', { type: 'saveuserfailed' })
-        })
-    } catch (err) { res.render('error', { type: 'hashfailed' }) }
+            } else { res.send('fail') }                             // otherwise, send fail
+
+
+        } catch (err) { res.send('fail') }
+    })
 })
 
-app.listen(8080);
+app.post('/signup', (req, res) => {                     // signup form post req
+    const data = req.body                                   // grab the data
+
+    fs.access('./userData/' + data.username + '.json', async (err) => {
+        if (err) {                                          // read the file, if error, it doesn't exist, new user can be created
+            try {
+                const hash = await argon2.hash(data.password)    // hash the password
+
+                const newUser = {                                // new user object
+                    username: data.username,
+                    hash: hash,
+                    name: data.name
+                }
+
+                fs.writeFile('./userData/' + data.username + '.json', JSON.stringify(newUser), (error) => {
+                    if (error) {                                 // save user data
+                        res.send('error')                            // apologize if things go wrong
+                    }
+
+                    res.send(createCookie(data.username))             // send session cookie
+                })
+
+            } catch (err) { res.send('error') }
+
+        } else { res.send('exists') }                       // file could be read, therefore user alread exists, new user cannot be created
+    })
+})
+
+app.listen(8080)
